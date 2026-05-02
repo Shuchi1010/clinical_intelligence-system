07868ce3f734acb06db5eacd2b945929d5bfa504
Shuchita Gupta <Shuchita.Gupta@gds.ey.com>
2026-05-03 03:24:20 +0530

    day 4 code completed

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
 # Day 2: Fetch Data from API
 import requests
-
 def fetch_studies():
+#API end point
     url = "https://clinicaltrials.gov/api/v2/studies"
-
+ #filters , studies limited to 10   
     params = {
         "query.term": "diabetes",
-        "pageSize": 10  # keep small for testing
+        "pageSize": 5  # keep small for testing
     }
+    response = requests.get(url,params=params)
+    data=response.json()
+    return data.get("studies",[])
 
-    response = requests.get(url, params=params)
-    data = response.json()
 
-    studies = []
-
-    for study in data.get("studies", []):
-        info = {
-            "nct_id": study.get("protocolSection", {})
-                .get("identificationModule", {})
-                .get("nctId"),
-
-            "title": study.get("protocolSection", {})
-                .get("identificationModule", {})
-                .get("briefTitle"),
-
-            "condition": study.get("protocolSection", {})
-                .get("conditionsModule", {})
-                .get("conditions"),
-
-            "status": study.get("protocolSection", {})
-                .get("statusModule", {})
-                .get("overallStatus")
-        }
-
-        studies.append(info)
-
-    return studies
